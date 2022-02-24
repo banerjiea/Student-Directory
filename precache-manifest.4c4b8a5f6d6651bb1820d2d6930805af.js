f3e4975f7ac5dcddcc7f6be5360613cbe813ca48
banerjiea <soubhik.hzs@gmail.com>
2022-02-24 12:18:56 +0530

    Updates

---
+++ precache-manifest.4c4b8a5f6d6651bb1820d2d6930805af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9973f075c17bb111e724d5b2291dd9ee",
+    "url": "/Student-Directory/index.html"
+  },
+  {
+    "revision": "6fe1e7dd23579df71f82",
+    "url": "/Student-Directory/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "b006f450e52d04e55c18",
+    "url": "/Student-Directory/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "6fe1e7dd23579df71f82",
+    "url": "/Student-Directory/static/js/2.bad6331b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Student-Directory/static/js/2.bad6331b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d726ec838de19b5cdb",
+    "url": "/Student-Directory/static/js/3.8b4c8e44.chunk.js"
+  },
+  {
+    "revision": "b006f450e52d04e55c18",
+    "url": "/Student-Directory/static/js/main.6244cd8e.chunk.js"
+  },
+  {
+    "revision": "8bddb9703d3eb11c978c",
+    "url": "/Student-Directory/static/js/runtime-main.d860e8c8.js"
+  }
+]);
